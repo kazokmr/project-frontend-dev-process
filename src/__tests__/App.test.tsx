c39dd0fec66dbc72f44fdd6b6a09b231c8a53985
Kazuhiko Okumura <engkokmr@gmail.com>
2022-02-26 11:05:46 +0900

    Todoアプリの表示テストをContainerからQueryを使ってテストするように変更した

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,21 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "../App";
 
-test("renders learn react link", () => {
-  const { container } = render(<App />);
-  const element = container.querySelector("div[class='todo-container']");
-  expect(element).toBeValid();
+test("Todoアプリ画面を表示する", () => {
+  render(<App />);
+  // やることの追加フォームの表示
+  expect(screen.getByPlaceholderText(/やることを/)).toBeInTheDocument();
+  // やることリストの表示
+  expect(screen.getByText("todo")).toBeInTheDocument();
+  // やることリストの操作エリアの表示
+  const operationTitles = [
+    "Actions",
+    "Remaining Todos",
+    "Filter by Status",
+    "Filter by Color",
+  ];
+  screen.getAllByRole("heading", { level: 5 }).forEach((value, index) => {
+    expect(value.textContent).toBe(operationTitles[index]);
+  });
 });
