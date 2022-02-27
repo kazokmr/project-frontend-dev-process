ca4ee63528e6fc03d9f25a9d9d639dbb10bedc28
Kazuhiko Okumura <engkokmr@gmail.com>
2022-02-28 06:17:51 +0900

    パラメタライズドテストに変更

--- src/__tests__/RemainingTodos.test.tsx
+++ src/__tests__/RemainingTodos.test.tsx
@@ -2,16 +2,16 @@ import { render, screen } from "@testing-library/react";
 import RemainingTodos from "../todo/operating/RemainingTodos";
 
 describe("TodoListの件数によるメッセージの違い", () => {
-  test("TodoListが1件なら、'1 remain left' と出力する", () => {
-    render(<RemainingTodos numOfTodo={1} />);
-    expect(screen.getByText("1 remain left")).toBeInTheDocument();
-  });
-  test("TodoListが2件なら、'2 remains left' と出力する", () => {
-    render(<RemainingTodos numOfTodo={2} />);
-    expect(screen.getByText("2 remains left")).toBeInTheDocument();
-  });
-  test("TodoListが0件なら、'0 remain left' と出力する", () => {
-    render(<RemainingTodos numOfTodo={0} />);
-    expect(screen.getByText("0 remain left")).toBeInTheDocument();
-  });
+  test.each`
+    numOfTodo | message
+    ${1}      | ${"1 remain left"}
+    ${2}      | ${"2 remains left"}
+    ${0}      | ${"0 remain left"}
+  `(
+    "TodoListが $numOfTodo なら' $message 'と出力する",
+    ({ numOfTodo, message }) => {
+      render(<RemainingTodos numOfTodo={numOfTodo} />);
+      expect(screen.getByText(message)).toBeInTheDocument();
+    }
+  );
 });
