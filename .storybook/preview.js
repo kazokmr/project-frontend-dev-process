1e3d111c151aa0c62276d16a2bd2227820db90af
Kazuhiko Okumura <engkokmr@gmail.com>
2022-03-26 21:37:30 +0900

    Storybookのアドオンを２つ追加
    - Snapshotを取得するためのアドオンとそのSnapshotsでテストするためのモジュール
    - StoryでMSWを使ったMockデータの取得が行えるアドオン

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,12 @@
+import "../src/index.css";
+import "../src/App.css";
+import "../src/todo/TodoApp.css";
+import { initialize, mswDecorator } from "msw-storybook-addon";
+
+initialize();
+
+export const decorators = [mswDecorator];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
