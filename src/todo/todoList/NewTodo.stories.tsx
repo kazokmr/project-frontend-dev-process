09e6ecf908c07fe304b5b9c83b746b119b97cdea
Kazuhiko Okumura <engkokmr@gmail.com>
2022-03-28 07:47:59 +0900

    TodoItemストーリーを追加
    - NewTodoのArgTypesの不要なフィールド設定は削除した

--- src/todo/todoList/NewTodo.stories.tsx
+++ src/todo/todoList/NewTodo.stories.tsx
@@ -3,19 +3,9 @@ import NewTodo from "./NewTodo";
 
 const meta: ComponentMeta<typeof NewTodo> = {
   component: NewTodo,
-  title: "Todoの入力",
   argTypes: {
     addTodo: {
-      name: "Todoの追加関数",
-      type: { name: "function", required: true },
       description: "テキストに入力した文字をTodoにする",
-      table: {
-        type: {
-          summary: "Todoを追加する関数",
-          detail: "入力文字をTodoにする",
-        },
-      },
-      control: { type: null },
       action: "Adding Todo",
     },
   },
