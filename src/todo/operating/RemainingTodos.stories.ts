0dce7177919c7094138eac916c899110410b92d7
Kazuhiko Okumura <engkokmr@gmail.com>
2022-03-30 06:27:04 +0900

    RemainingTodos.stories.ts を追加
    - numOfTodoの値をargTypesプロパティのControl:range を指定してスライダーで指定するようにしてみた

---
+++ src/todo/operating/RemainingTodos.stories.ts
@@ -0,0 +1,33 @@
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+import RemainingTodos from "./RemainingTodos";
+
+const Meta: ComponentMeta<typeof RemainingTodos> = {
+  component: RemainingTodos,
+};
+export default Meta;
+
+export const Default: ComponentStoryObj<typeof RemainingTodos> = {
+  storyName: "ActiveなTodoが１件",
+  args: {
+    numOfTodo: 1,
+  },
+};
+
+export const NoActiveTodo: ComponentStoryObj<typeof RemainingTodos> = {
+  storyName: "ActiveなTodoが0件",
+  args: {
+    numOfTodo: 0,
+  },
+};
+
+export const MultiActiveTodo: ComponentStoryObj<typeof RemainingTodos> = {
+  storyName: "ActiveなTodoが2件",
+  args: {
+    numOfTodo: 2,
+  },
+  argTypes: {
+    numOfTodo: {
+      control: { type: "range", min: 0, max: 100, step: 1 },
+    },
+  },
+};
