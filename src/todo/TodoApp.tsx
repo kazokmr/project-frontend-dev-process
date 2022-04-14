919cabedef4779caacd6abf099a4bd8caa66c71e
Kazuhiko Okumura <engkokmr@gmail.com>
2022-04-15 08:19:20 +0900

    TodoAppコンポーネントで子コンポーネントへのProps渡しを無くした
    - ReactQueryを使いuseQueryを実行することでServer Stateを管理するようにしている

--- src/todo/TodoApp.tsx
+++ src/todo/TodoApp.tsx
@@ -2,109 +2,40 @@ import "./TodoApp.css";
 import NewTodo from "./todoList/NewTodo";
 import TodoList from "./todoList/TodoList";
 import OperatingTodos from "./operating/OperatingTodos";
-import { Todo } from "./model/todo/Todo";
-import { useEffect, useRef, useState } from "react";
 import { TodoColor } from "./model/filter/TodoColors";
 import { TODO_STATUS, TodoStatus } from "./model/filter/TodoStatus";
-import axios from "axios";
+import { useQueryTodo } from "./hooks/useTodos";
+import { useQuery } from "react-query";
 
 const TodoApp = (): JSX.Element => {
-  const isMountRef = useRef<boolean>(false);
-  const [todos, setTodos] = useState<Todo[]>([] as Todo[]);
-  const [status, setStatus] = useState<TodoStatus>(TODO_STATUS.ALL);
-  const [colors, setColors] = useState<TodoColor[]>([] as TodoColor[]);
+  const { data: status } = useQuery<TodoStatus>(["status"], {
+    enabled: false,
+    staleTime: Infinity,
+    initialData: TODO_STATUS.ALL,
+  });
+  const { data: colors } = useQuery<TodoColor[]>(["colors"], {
+    enabled: false,
+    staleTime: Infinity,
+    initialData: [] as TodoColor[],
+  });
+  const { isLoading, isError } = useQueryTodo({
+    status,
+    colors,
+  });
 
-  const fetchTodo = async (): Promise<void> => {
-    const response = await axios.get("/todos");
-    const todos: Todo[] = response.data;
-    if (isMountRef.current) {
-      setTodos(todos);
-    }
-  };
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
 
-  const selectTodo: Todo[] = todos
-    .filter(
-      (todo) =>
-        status === TODO_STATUS.ALL ||
-        (status === TODO_STATUS.ACTIVE && !todo.isCompleted) ||
-        (status === TODO_STATUS.COMPLETED && todo.isCompleted)
-    )
-    .filter((todo) => colors.length === 0 || colors.includes(todo.color));
-
-  const addTodo = async (text: string): Promise<void> => {
-    const response = await axios.post("/todo", { text: text });
-    const todo = response.data;
-    setTodos([...todos, todo]);
-  };
-
-  const updateComplete = (id: string): void =>
-    setTodos(
-      todos.map((todo: Todo) =>
-        todo.id !== id ? todo : { ...todo, isCompleted: !todo.isCompleted }
-      )
-    );
-
-  const updateColor = (id: string, changingColor: TodoColor): void =>
-    setTodos(
-      todos.map((todo: Todo) =>
-        todo.id !== id ? todo : { ...todo, color: changingColor }
-      )
-    );
-
-  const deleteTodo = (id: string): void =>
-    setTodos(todos.filter((todo) => todo.id !== id));
-
-  const remainingTodos: number = todos.filter(
-    (todo) => !todo.isCompleted
-  ).length;
-
-  const filterByStatus = (status: TodoStatus): void => setStatus(status);
-
-  const filterByColors = (color: TodoColor, isSelected: boolean): void => {
-    if (isSelected && !colors.includes(color)) {
-      setColors([...colors, color]);
-    }
-    if (!isSelected && colors.includes(color)) {
-      setColors(colors.filter((selColor) => selColor !== color));
-    }
-  };
-
-  const completeAllTodos = (): void =>
-    setTodos(
-      todos.map((todo) =>
-        todo.isCompleted ? todo : { ...todo, isCompleted: true }
-      )
-    );
-
-  const clearCompleted = (): void =>
-    setTodos(todos.filter((todo) => !todo.isCompleted));
-
-  useEffect(() => {
-    isMountRef.current = true;
-    fetchTodo();
-    return () => {
-      isMountRef.current = false;
-    };
-  }, []);
+  if (isError) {
+    return <span>Error!!</span>;
+  }
 
   return (
     <div className="todo-container">
-      <NewTodo addTodo={addTodo} />
-      <TodoList
-        todos={selectTodo}
-        onChangeComplete={updateComplete}
-        onChangeColor={updateColor}
-        onClickDelete={deleteTodo}
-      />
-      <OperatingTodos
-        onClickMarkAllCompleted={completeAllTodos}
-        onClickClearCompleted={clearCompleted}
-        numberOfTodos={remainingTodos}
-        curStatus={status}
-        onClickStatus={filterByStatus}
-        curColors={colors}
-        onChangeColor={filterByColors}
-      />
+      <NewTodo />
+      <TodoList />
+      <OperatingTodos />
     </div>
   );
 };
--- src/todo/operating/OperatingTodos.tsx
+++ src/todo/operating/OperatingTodos.tsx
@@ -2,37 +2,14 @@ import ActionsForTodos from "./ActionsForTodos";
 import RemainingTodos from "./RemainingTodos";
 import StatusFilter from "./StatusFilter";
 import ColorFilter from "./ColorFilter";
-import { TodoColor } from "../model/filter/TodoColors";
-import { TodoStatus } from "../model/filter/TodoStatus";
 
-interface OperatingTodosProps {
-  onClickMarkAllCompleted: () => void;
-  onClickClearCompleted: () => void;
-  numberOfTodos: number;
-  curStatus: TodoStatus;
-  onClickStatus: (status: TodoStatus) => void;
-  curColors: TodoColor[];
-  onChangeColor: (color: TodoColor, isSelected: boolean) => void;
-}
-
-const OperatingTodos = ({
-  onClickMarkAllCompleted,
-  onClickClearCompleted,
-  numberOfTodos,
-  curStatus,
-  onClickStatus,
-  curColors,
-  onChangeColor,
-}: OperatingTodosProps): JSX.Element => {
+const OperatingTodos = (): JSX.Element => {
   return (
     <div className="todo-footer">
-      <ActionsForTodos
-        onClickMarkAllCompleted={onClickMarkAllCompleted}
-        onClickClearCompleted={onClickClearCompleted}
-      />
-      <RemainingTodos numOfTodo={numberOfTodos} />
-      <StatusFilter curStatus={curStatus} onClickStatus={onClickStatus} />
-      <ColorFilter curColors={curColors} onChangeColor={onChangeColor} />
+      <ActionsForTodos />
+      <RemainingTodos />
+      <StatusFilter />
+      <ColorFilter />
     </div>
   );
 };
