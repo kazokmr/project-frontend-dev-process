1ee2c0439e838a5e08282b0f0309de263c43e9b9
Kazuhiko Okumura <engkokmr@gmail.com>
2022-04-16 05:35:59 +0900

    App.test.tsx のテストが通るように修正
    - ReactQueryを有効にした
    - Loading表示対応でasync/await を追加

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "../App";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 describe("Todoアプリ画面の初期レンダリング", () => {
-  test("Todoアプリ画面を表示する", () => {
-    render(<App />);
+  test("Todoアプリ画面を表示する", async () => {
+    render(<QueryClientProvider client={new QueryClient()}><App /></QueryClientProvider>);
     // やることの追加フォームの表示
     expect(
-      screen.getByRole("textbox", { name: "input-todo" })
+      await screen.findByRole("textbox", { name: "input-todo" })
     ).toBeInTheDocument();
     // やることリストの表示
-    expect(screen.getByRole("list", { name: "list-todo" })).toBeInTheDocument();
+    expect(await screen.findByRole("list", { name: "list-todo" })).toBeInTheDocument();
     // やることリストの操作エリアの表示
     const operationTitles = [
       "Actions",
@@ -18,7 +19,7 @@ describe("Todoアプリ画面の初期レンダリング", () => {
       "Filter by Status",
       "Filter by Color",
     ];
-    screen.getAllByRole("heading", { level: 5 }).forEach((value, index) => {
+    (await screen.findAllByRole("heading", { level: 5 })).forEach((value, index) => {
       expect(value.textContent).toBe(operationTitles[index]);
     });
   });
