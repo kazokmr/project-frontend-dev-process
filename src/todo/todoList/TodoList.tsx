11fc35d90436575726f923ab2eb69b5e423ecc77
Kazuhiko Okumura <engkokmr@gmail.com>
2022-04-29 17:30:19 +0900

    TodoItem にもMUIスタイルを適用しました
    - ColorセレクタはNativeSelectを利用しました。
    - 理由はSelectにするとoption要素を含めaria-roleがcomboboxではなくなるため
    - 完了済みチェックはSwitch（トグル）にしませんでした。
    - これも理由は、完了したことがトグルだと明確ではなかったからです

--- src/todo/todoList/TodoList.tsx
+++ src/todo/todoList/TodoList.tsx
@@ -1,15 +1,16 @@
 import TodoItem from "./TodoItem";
 import { useFilteredTodos } from "../hooks/useTodos";
+import { List } from "@mui/material";
 
 const TodoList = (): JSX.Element => {
   const todos = useFilteredTodos().data ?? [];
 
   return (
-    <ul aria-label={"list-todo"}>
+    <List aria-label={"list-todo"} dense={true}>
       {todos.map((todo) => (
         <TodoItem key={todo.id} todo={todo} />
       ))}
-    </ul>
+    </List>
   );
 };
 
