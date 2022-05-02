bbb8dcf9adcccf6294937615d4748fdd886c15a1
Kazuhiko Okumura <24545571+kazokmr@users.noreply.github.com>
2022-05-03 02:11:32 +0900

    StatusFilterをRadioButtonに変更しました (#65)
    * NewTodoコンポーネントにStyledComponentを適用した
    
    - Operatingのmt指定が間違っていたので修正
    - 全体的にレイアウトを微調整して、widthの基準をmdにした
    
    * TodoItem にもMUIスタイルを適用しました
    
    - ColorセレクタはNativeSelectを利用しました。
    - 理由はSelectにするとoption要素を含めaria-roleがcomboboxではなくなるため
    - 完了済みチェックはSwitch（トグル）にしませんでした。
    - これも理由は、完了したことがトグルだと明確ではなかったからです
    
    * 依存関係のバージョンアップ
    
    * useTodosのHttp接続をインタフェースにし、特定のライブラリとの依存度を減らした
    
    - AxiosはHttpClientインタフェースを実装してRestClientコンテナに閉じ込めた
    
    * ライブラリのアップデート
    
    * StatusFilterの選択フォームをRadioButtonに変更した
    
    - Buttonだと現在選択しているフィルタ条件がわからなかったため。

--- src/todo/operating/StatusFilter.tsx
+++ src/todo/operating/StatusFilter.tsx
@@ -1,52 +1,48 @@
 import { TODO_STATUS, TodoStatus } from "../model/filter/TodoStatus";
 import { useRecoilState } from "recoil";
 import { statusFilterState } from "../TodoApp";
-import { Button, ButtonGroup, Container, Typography } from "@mui/material";
+import {
+  Container,
+  FormControl,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  Typography,
+} from "@mui/material";
+import { ChangeEvent } from "react";
 
 const StatusFilter = (): JSX.Element => {
   const [status, setStatus] = useRecoilState<TodoStatus>(statusFilterState);
 
   return (
     <Container maxWidth={"md"}>
-      <Typography variant={"subtitle1"} gutterBottom={true}>
-        Filter by Status
-      </Typography>
-      <ButtonGroup
-        variant={"contained"}
-        orientation={"vertical"}
-        color={"secondary"}
-      >
-        <Button
-          sx={{
-            typography: "button",
-            textTransform: "capitalize",
-          }}
-          aria-pressed={status === TODO_STATUS.ALL}
-          onClick={() => setStatus(TODO_STATUS.ALL)}
+      <FormControl>
+        <Typography variant={"subtitle1"} gutterBottom={true}>
+          Filter by Status
+        </Typography>
+        <RadioGroup
+          value={status}
+          onChange={(event: ChangeEvent<HTMLInputElement>) =>
+            setStatus(event.target.value as TodoStatus)
+          }
         >
-          {TODO_STATUS.ALL}
-        </Button>
-        <Button
-          sx={{
-            typography: "button",
-            textTransform: "capitalize",
-          }}
-          aria-pressed={status === TODO_STATUS.ACTIVE}
-          onClick={() => setStatus(TODO_STATUS.ACTIVE)}
-        >
-          {TODO_STATUS.ACTIVE}
-        </Button>
-        <Button
-          sx={{
-            typography: "button",
-            textTransform: "capitalize",
-          }}
-          aria-pressed={status === TODO_STATUS.COMPLETED}
-          onClick={() => setStatus(TODO_STATUS.COMPLETED)}
-        >
-          {TODO_STATUS.COMPLETED}
-        </Button>
-      </ButtonGroup>
+          <FormControlLabel
+            control={<Radio />}
+            value={TODO_STATUS.ALL}
+            label={TODO_STATUS.ALL}
+          />
+          <FormControlLabel
+            control={<Radio />}
+            value={TODO_STATUS.ACTIVE}
+            label={TODO_STATUS.ACTIVE}
+          />
+          <FormControlLabel
+            control={<Radio />}
+            value={TODO_STATUS.COMPLETED}
+            label={TODO_STATUS.COMPLETED}
+          />
+        </RadioGroup>
+      </FormControl>
     </Container>
   );
 };
