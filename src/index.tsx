41651736e0093ead1fe86979cc5aad7fcf2869f3
kazokmr <engkokmr@gmail.com>
2023-02-05 17:50:26 +0900

    Renderingを新しいAPIに変更した

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RecoilRoot } from "recoil";
 import { CssBaseline } from "@mui/material";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
 if (import.meta.env.DEV) {
@@ -13,8 +13,11 @@ if (import.meta.env.DEV) {
 }
 
 const queryClient = new QueryClient();
+const component = document.getElementById("root");
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(component!);
 
-render(
+root.render(
   <StrictMode>
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
@@ -23,6 +26,5 @@ render(
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </RecoilRoot>
-  </StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 );
