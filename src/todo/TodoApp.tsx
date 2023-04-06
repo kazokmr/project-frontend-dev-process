7aa32a7bc6902e1cc345bb6be082feb5a66abaf7
kazokmr <engkokmr@gmail.com>
2023-04-06 13:19:12 +0900

    `react-error-boundary` を使ってErrorBoundaryでエラーメッセージを出力するようにした

--- src/todo/TodoApp.tsx
+++ src/todo/TodoApp.tsx
@@ -1,19 +1,27 @@
 import { Container } from "@mui/material";
 import { Suspense } from "react";
+import { ErrorBoundary, FallbackProps } from "react-error-boundary";
 import NewTodo from "./todoList/NewTodo";
 import TodoList from "./todoList/TodoList";
 import OperatingTodos from "./operating/OperatingTodos";
 
+const FallbackError = ({ error }: FallbackProps) => (
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  <p>Error!!:{error.message}</p>
+);
+
 const TodoApp = (): JSX.Element => (
-  <Suspense fallback={<p>Loading...</p>}>
-    <Container maxWidth="md" component="main">
-      <NewTodo />
-      <TodoList />
-    </Container>
-    <Container maxWidth="md" component="footer" sx={{ mt: 6 }}>
-      <OperatingTodos />
-    </Container>
-  </Suspense>
+  <ErrorBoundary FallbackComponent={FallbackError}>
+    <Suspense fallback={<p>Loading...</p>}>
+      <Container maxWidth="md" component="main">
+        <NewTodo />
+        <TodoList />
+      </Container>
+      <Container maxWidth="md" component="footer" sx={{ mt: 6 }}>
+        <OperatingTodos />
+      </Container>
+    </Suspense>
+  </ErrorBoundary>
 );
 
 export default TodoApp;
