472032ff775f769213823324c3740103401d1d9b
kazokmr <engkokmr@gmail.com>
2023-12-02 10:56:02 +0900

    テストかバッレジ対象を指定。 ``.stories.tsx` を除外するようにしたがGlobパターンの書き方が適切かは要検討

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
       provider: "v8",
       reporter: ["text", "html"],
       reportsDirectory: "./coverage",
+      include: ["**/src/**/!(*.stories)*.[jt]s?(x)"],
     },
     setupFiles: ["./vitest.setup.ts"],
   },
