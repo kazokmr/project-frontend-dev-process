becc6d509e14889615628a89ad752b60d105e701
kazokmr <engkokmr@gmail.com>
2024-01-31 23:17:36 +0900

    update dependencies. msw-storybook-addon の 更新によってvite.configの workaroundが不要になったので削除した

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
-import path from "node:path";
 
 export default defineConfig({
   plugins: [react()],
@@ -16,11 +15,5 @@ export default defineConfig({
       include: ["**/src/**/!(*.stories)*.[jt]s?(x)"],
     },
     setupFiles: ["./vitest.setup.ts"],
-  },
-  // TODO msw-storybook-addon 2.0.0-beta.0 の Bugのworkaround 解消したら消す
-  resolve: {
-    alias: {
-      "msw/native": path.resolve(__dirname, "./node_modules/msw/lib/native/index.mjs"),
-    },
   },
 });
